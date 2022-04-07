5309715cc2bd841a9ccc1d4bb9ca48b5ace903f4
den3606 <dedendendedenpunn@gmail.com>
2022-04-07 19:19:00 +0900

    feat: 初期画面表示をデカくした

--- src/main.js
+++ src/main.js
@@ -5,8 +5,8 @@ app.disableHardwareAcceleration()
 
 const createWindow = () => {
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1200,
+    height: 800,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
       webSecurity: false,
